a7464e2a4365c7b75f4d3c4bcc123ffed41f2d80
Oskar Hagen <oskar.hagen@outlook.com>
2026-03-11 22:41:56 -0300

    Publish site update

---
+++ docs/assets/materials/README.md
@@ -0,0 +1,10 @@
+# Course Materials Bundle
+
+This folder contains the local, course-organized copy of the teaching material used in the SoSe 2026 block seminar.
+
+## Structure
+
+- `day1/`: setup, config basics, island introduction material and configs.
+- `day2/`: output analysis, landscape building material, and Day 2 datasets.
+- `day3/`: sandbox, critical reflection notes, advanced configs, and scripts.
+- `shared/integrated_core/`: cross-day integrated workshop files and configs.
---
+++ docs/assets/materials/day1/configs/config_islands_simple_Day1Prac3_M3.R
@@ -0,0 +1,132 @@
+########################
+### General settings ###
+########################
+
+random_seed = 663
+start_time = 50
+end_time = NA
+max_number_of_species =50000
+max_number_of_coexisting_species =20000
+initial_abundance =  1
+
+# set for first time
+# assign("ss_eff", ss_eff_emp, envir = .GlobalEnv)
+# a list of traits to include with each species, traits with ss_eff_ are hack traits to extract in cell processes
+trait_names = c("dispersal", "temp_niche_centre", "temp_niche_width", "start_island")
+
+# ranges to scale the input environemts with:
+# not listed variable:         no scaling takes place
+# listed, set to NA:           the environmental variable will be scaled from [min, max] to [0, 1]
+# listed with a given range r: the environmental variable will be scaled from [r1, r2] to [0, 1]
+
+# environmental_ranges = list("mean_temp"=c(9,26), "min_temp"=c(9,26),  "max_temp"=c(9,26))
+
+#########################
+### Observer Function ###
+#########################
+
+end_of_timestep_observer = function(data, vars, config){  
+  
+  plot_richness(data$all_species, data$landscape)
+  
+  save_species() # saves a species and landscape objects for desired timesteps
+  
+}
+
+
+######################
+### Initialization ###
+######################
+create_ancestor_species <- function(landscape, config) {
+  co <- landscape$coordinates
+  # get patches vector
+  pv <- landscape$environment[, "patch"]
+
+  new_species <- list()
+  
+  manual_traits <- list(
+    "dispersal" = c(10, 7.5, 5, 2.5),
+    "temp_niche_width" = c(0.5, 1, 2, 4)
+  )
+  
+  for (sp_i in 1:4){
+    # create a species empty object
+    new_species[[sp_i]] <- create_species(names(pv[pv==sp_i]), config)
+    # set manual dispersal and niche width
+    new_species[[sp_i]]$traits[ , "dispersal"] <- manual_traits$"dispersal"[sp_i]
+    new_species[[sp_i]]$traits[ , "temp_niche_width"] <- manual_traits$"temp_niche_width"[sp_i]
+    # set species 1 start to island 1, species 2 to island 2, etc...
+    new_species[[sp_i]]$traits[ , "start_island"] <- unique(landscape$environment[pv==sp_i, "patch"]) # this is just a sanity check
+    # set species mean temp to the mean temp of the patches
+    new_species[[sp_i]]$traits[ , "temp_niche_centre"] <- mean(landscape$environment[pv==sp_i, "mean_temp"])
+  }
+  
+  return(new_species)
+}
+
+
+#################
+### Dispersal ###
+#################
+
+# returns n dispersal values (prob distrib function)
+get_dispersal_values <- function(n, species, landscape, config){
+  #browser()
+  values <- rexp(n, rate = 1/species$traits[,"dispersal"])
+  return(values)
+}
+
+
+##################
+### Speciation ###
+##################
+
+# threshold for genetic distance after which a speciation event takes place
+divergence_threshold=5 # between 10 and 50 ? as 0.1 to 0.5 Myrs or 100 - 500 kyrs
+
+# adds a value of 1 to each geographic population cluster
+get_divergence_factor <- function(species, cluster_indices, landscape, config) {
+  return(1)
+}
+
+#######################
+### Trait Evolution ###
+#######################
+
+apply_evolution <- function(species, cluster_indices, landscape, config) {
+  # cell names
+  trait_evolutionary_power <-0.05
+  traits <- species[["traits"]]
+  cells <- rownames(traits)
+  #homogenize trait based on abundance
+  # Homogenize all traits by weighted abundance, attention, exclude here any trait that should be more neutral
+  trn <- config$gen3sis$general$trait_names
+  for(cluster_index in unique(cluster_indices)){
+    # cluster_index <- 1
+    cells_cluster <- cells[which(cluster_indices == cluster_index)]
+    # hist(traits[cells_cluster, "temp"], main="before")
+    mean_abd <- mean(species$abundance[cells_cluster])
+    weight_abd <- species$abundance[cells_cluster]/mean_abd
+    for (ti in trn){
+      traits[cells_cluster, ti] <- mean(traits[cells_cluster, ti]*weight_abd)
+    }
+    # hist(traits[cells_cluster, "temp"], main="after")
+  }
+  
+  # mutate mean temperature
+  mutation_deltas <-rnorm(length(traits[, ti]), mean=0, sd=trait_evolutionary_power)
+  traits[, "temp_niche_centre"] <- traits[, "temp_niche_centre"] + mutation_deltas
+  return(traits)
+}
+#################################################
+### Environmental and Ecological Interactions ###
+#################################################
+
+apply_ecology <- function(abundance, traits, landscape, config) {
+  # get the difference between species and site mean temp
+  diff <- abs(traits[, "temp_niche_centre"]-landscape[,"mean_temp"])
+  # set the abundance of species with a difference in mean temp larger than the species temp width to zero and the ones below to one
+  abundance[diff>traits[,"temp_niche_width"]] <- 0
+  abundance[diff<=traits[,"temp_niche_width"]] <- 1
+  return(abundance)
+}
